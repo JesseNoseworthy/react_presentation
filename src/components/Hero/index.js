201c5862de054af45ab30a0e722cf6e553c7c68d
Jesse Noseworthy <noseworthyjesse@gmail.com>
2016-12-28 15:26:45 -0500

    simple hero

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import styles from './Hero.scss';
+import Image from '../Image';
 
 class Hero extends Component {
   render() {
     return (
       <div className={styles.heroContainer}>
+        <Image variant="hero" src={require('../../app/assets/react-logo.png')} />
         <h1>React Presentation</h1>
       </div>
     )
