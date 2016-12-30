48fee7881961330debb3a385d68fdc482956c07f
Jesse Noseworthy <noseworthyjesse@gmail.com>
2016-12-30 16:35:46 -0500

    rough

--- src/components/Slide/index.js
+++ src/components/Slide/index.js
@@ -6,15 +6,23 @@ class Slide extends Component {
     const {
       data,
       variant,
+      title,
+      subTitle,
       ...others
     } = this.props;
 
     return (
-      <div className="react-slides">
-        {Object.keys(data).map(function(i) {
+      <div className="react-slides" className={styles.slide}>
+        <h1>{title}</h1>
+        {Object.keys(subTitle).map(function(i) {
+          const a = subTitle[i].points;
+          console.log(a);
           return (
-            <div key={i} className={styles.slide}>
-              <h1>{data[i].title}</h1>
+            <div key={i}>
+              <h4>{subTitle[i].title}</h4>
+              <ul>
+                <li>{subTitle[i].points.split()}</li>
+              </ul>
             </div>
           )
         })}
