45f11456a21e58552f71ed3f56b95c22725aab48
Jesse Noseworthy <noseworthyjesse@gmail.com>
2017-02-05 15:25:39 -0500

    Image imports are better this way

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import styles from './Hero.scss';
 import Image from '../Image';
+import reactLogo from '../../app/assets/react-logo.png';
 
 const Hero = () => (
   <div className={styles.heroContainer}>
-    <Image variant="hero" src={require('../../app/assets/react-logo.png')} />
+    <Image variant="hero" src={reactLogo} />
     <h1>An Introduction to <span className={styles.reactHighlight}>React</span></h1>
   </div>
 );
