d3af3ef1ced2fecb84fef576733f360fcda2ed65
Jesse Noseworthy <noseworthyjesse@gmail.com>
2017-02-05 15:23:16 -0500

    Non-essentials

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -2,15 +2,11 @@ import React, { Component } from 'react';
 import styles from './Hero.scss';
 import Image from '../Image';
 
-class Hero extends Component {
-  render() {
-    return (
-      <div className={styles.heroContainer}>
-        <Image variant="hero" src={require('../../app/assets/react-logo.png')} />
-        <h1>An Introduction to <span className={styles.reactHighlight}>React</span></h1>
-      </div>
-    )
-  }
-}
+const Hero = () => (
+  <div className={styles.heroContainer}>
+    <Image variant="hero" src={require('../../app/assets/react-logo.png')} />
+    <h1>An Introduction to <span className={styles.reactHighlight}>React</span></h1>
+  </div>
+);
 
 export default Hero;
